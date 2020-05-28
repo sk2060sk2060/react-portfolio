f4701086062aa495f17e4806dc2b3e3557062b21
sk <sk2060sk2060@gmail.com>
2020-05-27 23:51:54 -0600

    Updates

---
+++ precache-manifest.9ffec226a2bb0e611bb3667e9d1ca03a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab438343503b7941ed2ab76564ca4ec",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "dcfdcfc7d7a8c71eb1e1",
+    "url": "/react-portfolio/static/css/main.2a1e8f8c.chunk.css"
+  },
+  {
+    "revision": "9ba6f88ebc811766a3f7",
+    "url": "/react-portfolio/static/js/2.7b1680c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.7b1680c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcfdcfc7d7a8c71eb1e1",
+    "url": "/react-portfolio/static/js/main.6eb0352d.chunk.js"
+  },
+  {
+    "revision": "bee14ebf850387594d39",
+    "url": "/react-portfolio/static/js/runtime-main.8c64cdeb.js"
+  },
+  {
+    "revision": "bec4ec6f2028675a02388360de6566e5",
+    "url": "/react-portfolio/static/media/budget-tracker.bec4ec6f.png"
+  },
+  {
+    "revision": "e4cbab97840780e8f475c88185b98ecf",
+    "url": "/react-portfolio/static/media/burger-pic.e4cbab97.jpg"
+  },
+  {
+    "revision": "78d0d86d80371c6230bffe38349c97a2",
+    "url": "/react-portfolio/static/media/code-quiz-2.78d0d86d.png"
+  },
+  {
+    "revision": "f2ae5db2b85d14a2a3ddd7124b2af684",
+    "url": "/react-portfolio/static/media/note-taker.f2ae5db2.png"
+  },
+  {
+    "revision": "4647b0e808bb01a90f5306e4bb0c6343",
+    "url": "/react-portfolio/static/media/weather-dashboard.4647b0e8.gif"
+  },
+  {
+    "revision": "1e7c2918cb56f76fb4c51cdcc3b1214e",
+    "url": "/react-portfolio/static/media/workout-tracker.1e7c2918.jpg"
+  }
+]);
